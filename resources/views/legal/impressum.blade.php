13e1f7a9f5f76abc9f2445f2f9076487dfe36052
Flip <philippe@pilarkto.net>
2022-02-25 20:57:09 +0100

    add ui template, auth

---
+++ resources/views/legal/impressum.blade.php
@@ -0,0 +1,13 @@
+<div class="card">
+    <div class="body">
+        <h1>Impressum</h1>
+
+        <p>Philippe Käufer
+            <br/>Lembergstrasse 11
+            <br/>70825 Korntal</p>
+        <p>Telefon: 0049 152 52075092</p>
+        <p>E-Mail: dienstplan@philhil.de</p>
+        <p>Inhaltlich Verantwortlicher gem. &sect; 55 II RStV: Philippe Käufer (Anschrift s.o.)</p>
+
+    </div>
+</div>
