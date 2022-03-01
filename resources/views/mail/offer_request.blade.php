5f6ca0071199b8e9fc42e7cd329595f31a9131d2
Flip <philippe@pilarkto.net>
2022-03-01 21:09:32 +0100

    email fix

--- resources/views/mail/offer_request.blade.php
+++ resources/views/mail/offer_request.blade.php
@@ -3,7 +3,7 @@ Hi {{$offer->user->first_name}},
 <br>
 <b>{{$requester_name}}</b> {{__('has sent a message to your offer:')}} <a href="{{route('offer.show', [$offer->id])}}">{{$offer->title}}</a>
 <br>
-{{__('If your offer is still available, please directly get in touch with')}} <b>{{$offer->user->first_name}}</b>. {{__('We advise you to only to reply to the e-mail address for this and not to open any links from the message')}}.
+{{__('If your offer is still available, please directly get in touch with')}} <b>{{$requester_name}}</b>. {{__('We advise you to only to reply to the e-mail address for this and not to open any links from the message')}}.
 <br><br>
 {{__('If your offer is no longer available, you can remove it by clicking here')}}: <a href="{{route('offer.delete', [$offer->id])}}">{{route('offer.delete', [$offer->id])}}</a>
 <br><br>
