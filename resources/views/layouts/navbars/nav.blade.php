37caae4aafb70ae027d9e8f9d8ea7b7aa4bdd583
Flip <philippe@pilarkto.net>
2022-03-05 20:07:38 +0100

    localization #5

--- resources/views/layouts/navbars/nav.blade.php
+++ resources/views/layouts/navbars/nav.blade.php
@@ -1,8 +1,7 @@
 <main class="main-content border-radius-lg">
-    <nav class="navbar navbar-main navbar-expand-lg px-0 mx-4 shadow-none border-radius-xl" id="navbarBlur"
-         navbar-scroll="true">
+    <nav class="navbar navbar-main navbar-expand-lg px-0 mx-4 shadow-none border-radius-xl" id="navbarBlur">
         <div class="container-fluid py-1 px-3">
-            @if (app('router')->getRoutes()->match(app('request')->create(app('url')->previous()))->getName() != "welcome")
+            @if (!in_array(request()->route()->getName(),['welcome']))
                 <nav aria-label="breadcrumb">
                     <ol class="breadcrumb bg-transparent mb-0 pb-0 pt-1 px-0 me-sm-6 me-5">
                         <li class="breadcrumb-item text-md"><a class="opacity-5 text-dark" href="javascript:;">Pages</a>
