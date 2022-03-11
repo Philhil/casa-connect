0a7c3fa14ce2933a56510afa98b108318872d753
Flip <philippe@pilarkto.net>
2022-03-11 19:48:15 +0100

    add metrics

--- resources/views/livewire/dashboard.blade.php
+++ resources/views/livewire/dashboard.blade.php
@@ -8,6 +8,28 @@
                         <div class="row">
                             <div class="col-8">
                                 <div class="numbers">
+                                    <p class="text-sm mb-0 text-capitalize font-weight-bold">{{__('Available beds')}}</p>
+                                    <h5 class="font-weight-bolder mb-0">
+                                        {{\App\Models\Offer::sum('amount')}}
+                                    </h5>
+                                </div>
+                            </div>
+                            <div class="col-4 text-end">
+                                <div class="icon icon-shape bg-gradient-primary shadow text-center border-radius-md">
+                                    <i class="ni ni-bold-right text-lg opacity-10" aria-hidden="true"></i>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-xl-3 col-sm-6 mb-xl-0 mb-4">
+                <div class="card">
+                    <div class="card-body p-3">
+                        <div class="row">
+                            <div class="col-8">
+                                <div class="numbers">
                                     <p class="text-sm mb-0 text-capitalize font-weight-bold">{{__('Total offers')}}</p>
                                     <h5 class="font-weight-bolder mb-0">
                                         {{$total_offers}}
@@ -17,6 +39,28 @@
                             <div class="col-4 text-end">
                                 <div class="icon icon-shape bg-gradient-primary shadow text-center border-radius-md">
                                     <i class="ni ni-world text-lg opacity-10" aria-hidden="true"></i>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-xl-3 col-sm-6 mb-xl-0 mb-4">
+                <div class="card">
+                    <div class="card-body p-3">
+                        <div class="row">
+                            <div class="col-8">
+                                <div class="numbers">
+                                    <p class="text-sm mb-0 text-capitalize font-weight-bold">{{__('Supporters')}}</p>
+                                    <h5 class="font-weight-bolder mb-0">
+                                        {{\App\Models\User::count()}}
+                                    </h5>
+                                </div>
+                            </div>
+                            <div class="col-4 text-end">
+                                <div class="icon icon-shape bg-gradient-primary shadow text-center border-radius-md">
+                                    <i class="ni ni-like-2 text-lg opacity-10" aria-hidden="true"></i>
                                 </div>
                             </div>
                         </div>
